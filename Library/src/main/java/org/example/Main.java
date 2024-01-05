40f2a5a8d91795a9999f47d7aa79b94a2df29a2d
Alexandru Dorian Muresan <126787359+alexutzu312@users.noreply.github.com>
2024-01-05 12:45:09 +0200

    Add files via upload

---
+++ Library/src/main/java/org/example/Main.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import database.DatabaseConnectionFactory;
+import database.JDBConnectionWrapper;
+import model.Book;
+import model.builder.BookBuilder;
+import repository.*;
+import service.BookService;
+import service.BookServiceImpl;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+
+        BookRepository bookRepository = new BookRepositoryCacheDecorator(
+                new BookRepositoryMySQL(DatabaseConnectionFactory.getConnectionWrapper(true).getConnection()),
+                new Cache<>()
+        );
+
+        BookService bookService = new BookServiceImpl(bookRepository);
+
+        Book book = new BookBuilder()
+                .setAuthor("Cezar Petrescu")
+                .setTitle("Fram Ursul Polar")
+                .setPublishedDate(LocalDate.of(2010, 6, 2))
+                .build();
+
+        bookService.save(book);
+
+        System.out.println(bookService.findAll());
+
+        System.out.println(bookService.findAll());
+        System.out.println(bookService.getAgeOfBook(2L));
+    }
+}
